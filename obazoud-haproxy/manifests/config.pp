23d459075e5a75336d96ee7728324f91769b02ec
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-09-16 20:03:35 +0200

    WIP

--- obazoud-haproxy/manifests/config.pp
+++ obazoud-haproxy/manifests/config.pp
@@ -1,10 +1,9 @@
 # == Class: haproxy::config
 #
-#
+# test
 class haproxy::config inherits haproxy {
 
   file { '/etc/haproxy/haproxy.cfg':
-    path    => '/etc/ssh/sshd_config',
     owner   => root,
     group   => root,
     mode    => '0644',
--- obazoud-haproxy/spec/acceptance/basic_spec.rb
+++ obazoud-haproxy/spec/acceptance/basic_spec.rb
@@ -3,13 +3,33 @@ require 'spec_helper_acceptance'
 describe 'configuring haproxy' do
   describe 'listen ports' do
 
-    describe port('9090') do
-      it { should_not be_listening }
+    it 'should be able to listen ports' do
+      pp = <<-EOS
+      class { 'haproxy':
+      }
+      EOS
+      apply_manifest(pp, :catch_failures => true)
     end
 
-    describe port('9091') do
-      it { should_not be_listening }
+    describe service('haproxy') do
+      it { should be_running }
     end
+
+    describe port('80') do
+      it { should be_listening }
+    end
+
+    describe port('22002') do
+      it { should be_listening }
+    end
+
+    describe command('/usr/bin/curl -s -i http://localhost:22002/') do
+      it { should return_exit_status 0 }
+      its(:stdout) { 
+        should match /HTTP\/1\.0 200/
+        should match /Statistics Report for HAProxy/
+      }
+    end
+
   end
-
 end
